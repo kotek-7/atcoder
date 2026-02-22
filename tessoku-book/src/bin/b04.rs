a08899fdfcc30b72b861709b01f892c37573d18d
kotek <scpktk@gmail.com>
2026-02-23 00:22:00 +0900

    b04

--- tessoku-book/src/bin/b04.rs
+++ tessoku-book/src/bin/b04.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {n: usize}
+    let digits = n
+        .to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect::<Vec<_>>();
+
+    let mut digit = digits.len();
+    let ans = digits.into_iter().fold(0, |prev, d| {
+        digit -= 1;
+        return prev + d * 2_u32.pow(digit as u32);
+    });
+
+    println!("{}", ans);
 }
