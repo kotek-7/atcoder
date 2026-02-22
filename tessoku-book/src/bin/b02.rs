6387a72473d495f6648b390579efa952b0aa5b3a
kotek <scpktk@gmail.com>
2026-02-22 23:00:06 +0900

    b02

--- tessoku-book/src/bin/b02.rs
+++ tessoku-book/src/bin/b02.rs
@@ -1,3 +1,15 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        a: usize,
+        b: usize
+    }
+    for i in a..=b {
+        if 100 % i == 0 {
+            println!("Yes");
+            break;
+        }
+        if i == b {
+            println!("No");
+        }
+    }
 }
