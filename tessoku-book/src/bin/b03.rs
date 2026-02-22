18ecfb248bb38c02d5a591d27f8137b17e1c74e4
kotek <scpktk@gmail.com>
2026-02-22 23:53:34 +0900

    b03

--- tessoku-book/src/bin/b03.rs
+++ tessoku-book/src/bin/b03.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a_s: [usize; n]
+    }
+
+    let mut found = false;
+    'outer: for a0 in &a_s {
+        for a1 in &a_s {
+            for a2 in &a_s {
+                if a0 + a1 + a2 == 1000 {
+                    found = true;
+                    break 'outer;
+                }
+            }
+        }
+    }
+    
+    if found {
+        println!("Yes")
+    } else {
+        println!("No")
+    }
 }
