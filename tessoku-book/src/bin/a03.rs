4206e0580aa4631913120acd797f675dba5b5c87
kotek <scpktk@gmail.com>
2026-02-22 23:24:37 +0900

    a03

--- tessoku-book/src/bin/a03.rs
+++ tessoku-book/src/bin/a03.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        p_s: [usize; n],
+        q_s: [usize; n]
+    }
+
+    'outer: for (i, p) in p_s.iter().enumerate() {
+        for q in q_s.iter() {
+            if p + q == k {
+                println!("Yes");
+                break 'outer;
+            }
+        }
+        if i == p_s.len() - 1 {
+            println!("No");
+        }
+    }
 }
