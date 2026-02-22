cb6b3603f6daf6b2808e7ea4f25585dbbf8d083a
kotek <scpktk@gmail.com>
2026-02-23 01:04:36 +0900

    a06

--- tessoku-book/src/bin/a06.rs
+++ tessoku-book/src/bin/a06.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n:usize, q: usize,
+        a_s: [usize; n],
+        lr_s: [(usize, usize); q]
+    }
+
+    let mut sums = Vec::with_capacity(n);
+    sums.push(0);
+    let mut sum_temp = 0;
+    for a in a_s {
+        sum_temp += a;
+        sums.push(sum_temp);
+    }
+
+    for (l, r) in lr_s {
+        println!("{}", sums[r] - sums[l - 1]);
+    }
 }
