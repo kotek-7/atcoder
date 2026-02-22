e40427b72d96f0e19428b396d9244aedabb18b92
kotek <scpktk@gmail.com>
2026-02-23 00:40:07 +0900

    a05

--- tessoku-book/src/bin/a05.rs
+++ tessoku-book/src/bin/a05.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize
+    }
+
+    let mut count = 0;
+
+    for a in 1..=n {
+        for b in 1..=n {
+            if k.checked_sub(a + b).is_some_and(|v| v <= n) && k != (a + b) {
+                count += 1;
+            }
+        }
+    }
+
+    println!("{}", count);
 }
