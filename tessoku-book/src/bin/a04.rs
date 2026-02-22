0eae57c0dddcf15087cf76904c55e865cc43601c
kotek <scpktk@gmail.com>
2026-02-23 00:05:16 +0900

    a04

--- tessoku-book/src/bin/a04.rs
+++ tessoku-book/src/bin/a04.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize
+    }
+    println!("{:0>10}", f(n));
+}
+
+fn f(n: usize) -> usize {
+    if n == 0 {
+        return 0;
+    }
+    return f(n / 2) * 10 + n % 2;
 }
